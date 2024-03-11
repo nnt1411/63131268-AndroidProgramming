e84c22ece88f78b8343d8f14ab21d15bfb118e1a
nnt1411 <thanh.nn.63cntt@ntu.edu.vn>
2024-03-11 13:25:07 +0700

    ex 5 var

---
+++ Ex5_AddSubMulDiv_Var/app/src/main/java/thanh/nn/MainActivity.java
@@ -0,0 +1,125 @@
+package thanh.nn;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editTextSo1;
+    EditText editTextSo2;
+    EditText editTextKQ;
+    Button nutCong,nutTru,nutNhan,nutChia;
+
+    void TimDieuKhien(){
+        editTextSo1 = (EditText)findViewById(R.id.edtSo1);
+        editTextSo2 = (EditText)findViewById(R.id.edtSo2);
+        editTextKQ = (EditText)findViewById(R.id.edtKQ);
+        nutCong = (Button)findViewById(R.id.btnCong);
+        nutTru = (Button)findViewById(R.id.btnTru);
+        nutNhan = (Button)findViewById(R.id.btnNhan);
+        nutChia = (Button)findViewById(R.id.btnChia);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        TimDieuKhien();
+
+        View.OnClickListener BolangNgheCong = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String strSo1 = editTextSo1.getText().toString();
+                String strSo2 = editTextSo2.getText().toString();
+
+                float So1 = Float.parseFloat(strSo1);
+                float So2 = Float.parseFloat(strSo2);
+
+                float tong = So1 + So2;
+
+                EditText editTextKQ = (EditText)findViewById(R.id.edtKQ);
+                String strKQ = String.valueOf(tong);
+                editTextKQ.setText(strKQ);
+
+            }
+        };
+
+        View.OnClickListener BolangNgheTru = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String strSo1 = editTextSo1.getText().toString();
+                String strSo2 = editTextSo2.getText().toString();
+
+                float So1 = Float.parseFloat(strSo1);
+                float So2 = Float.parseFloat(strSo2);
+
+                float tong = So1 - So2;
+
+                EditText editTextKQ = (EditText)findViewById(R.id.edtKQ);
+                String strKQ = String.valueOf(tong);
+                editTextKQ.setText(strKQ);
+
+            }
+        };
+
+        View.OnClickListener BolangNgheNhan = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String strSo1 = editTextSo1.getText().toString();
+                String strSo2 = editTextSo2.getText().toString();
+
+                float So1 = Float.parseFloat(strSo1);
+                float So2 = Float.parseFloat(strSo2);
+
+                float tong = So1 * So2;
+
+                EditText editTextKQ = (EditText)findViewById(R.id.edtKQ);
+                String strKQ = String.valueOf(tong);
+                editTextKQ.setText(strKQ);
+
+            }
+        };
+
+        View.OnClickListener BolangNgheChia = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String strSo1 = editTextSo1.getText().toString();
+                String strSo2 = editTextSo2.getText().toString();
+
+                float So1 = Float.parseFloat(strSo1);
+                float So2 = Float.parseFloat(strSo2);
+
+                float tong = So1 / So2;
+
+                EditText editTextKQ = (EditText)findViewById(R.id.edtKQ);
+                String strKQ = String.valueOf(tong);
+                editTextKQ.setText(strKQ);
+
+            }
+        };
+
+        nutCong.setOnClickListener(BolangNgheCong);
+        nutTru.setOnClickListener(BolangNgheTru);
+        nutNhan.setOnClickListener(BolangNgheNhan);
+        nutChia.setOnClickListener(BolangNgheChia);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
