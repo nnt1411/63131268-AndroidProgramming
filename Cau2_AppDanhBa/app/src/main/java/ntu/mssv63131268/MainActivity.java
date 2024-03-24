ba108631795db27918ef988548a8428cc11bd1bf
nnt1411 <thanh.nn.63cntt@ntu.edu.vn>
2024-03-24 15:39:03 +0700

    Tạo layout main và file java activity main

--- Cau2_AppDanhBa/app/src/main/java/ntu/mssv63131268/MainActivity.java
+++ Cau2_AppDanhBa/app/src/main/java/ntu/mssv63131268/MainActivity.java
@@ -1,12 +1,18 @@
 package ntu.mssv63131268;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +22,23 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        
+        ListView lvDSDanhBa = (ListView) findViewById(R.id.lvDanhBa);
+        ArrayList<DanhBa> dsDanhBa = new ArrayList<DanhBa>();
+        dsDanhBa.add(new DanhBa("Thành","0123456789",R.drawable.avatar));
+        dsDanhBa.add(new DanhBa("Hành","0987654321",R.drawable.avatar));
+        dsDanhBa.add(new DanhBa("Bành","017852369",R.drawable.avatar));
+        dsDanhBa.add(new DanhBa("Sành","0369852147",R.drawable.avatar));
+
+        DanhBaAdapter adapter = new DanhBaAdapter(this,dsDanhBa);
+        lvDSDanhBa.setAdapter(adapter);
+
+        lvDSDanhBa.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                DanhBa danhBaChon = dsDanhBa.get(position);
+                Toast.makeText(MainActivity.this,danhBaChon.getTen(),Toast.LENGTH_SHORT).show();
+            }
+        });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
