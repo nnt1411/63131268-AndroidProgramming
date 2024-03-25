f2dc2d5dfc67810a6ba71a31e7842deaa3cfa7cf
nnt1411 <thanh.nn.63cntt@ntu.edu.vn>
2024-03-25 12:21:16 +0700

    Thêm layout để hiện thông tin

--- Cau3_AppGioiThieu/app/src/main/java/ntu/mssv63131268/ThongTinSV.java
+++ Cau3_AppGioiThieu/app/src/main/java/ntu/mssv63131268/ThongTinSV.java
@@ -8,16 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class ThongTinSV extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
-
-
+        setContentView(R.layout.activity_thong_tin_sv);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
