fb2de00ebaf3b85acfe19e59a01dde82b817951f
nnt1411 <thanh.nn.63cntt@ntu.edu.vn>
2024-03-25 12:40:20 +0700

    Thêm thông tin vào listview

--- Cau3_AppGioiThieu/app/src/main/java/ntu/mssv63131268/ThongTinSV.java
+++ Cau3_AppGioiThieu/app/src/main/java/ntu/mssv63131268/ThongTinSV.java
@@ -1,12 +1,15 @@
 package ntu.mssv63131268;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
 
 public class ThongTinSV extends AppCompatActivity {
 
@@ -16,7 +19,16 @@ public class ThongTinSV extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_thong_tin_sv);
 
+        ListView lvThongTin = (ListView) findViewById(R.id.lvThongTin);
+        ArrayList<ThongTin> dsThongTin = new ArrayList<ThongTin>();
+        dsThongTin.add(new ThongTin("Tên:","Nguyễn Ngọc Thành"));
+        dsThongTin.add(new ThongTin("MSSV:","63131268"));
+        dsThongTin.add(new ThongTin("Email::","thanh.nn.63cntt@ntu.edu.vn"));
+        dsThongTin.add(new ThongTin("Ngày sinh:","14/11/2003"));
+        dsThongTin.add(new ThongTin("Quê Quán:","Khánh Hòa"));
 
+        ThongTinAdapter adapter = new ThongTinAdapter(this,dsThongTin);
+        lvThongTin.setAdapter(adapter);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
