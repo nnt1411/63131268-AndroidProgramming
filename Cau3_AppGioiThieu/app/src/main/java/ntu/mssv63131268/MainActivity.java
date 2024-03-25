0a6229da83008019be8d2a292d58aff2b9c4868f
nnt1411 <thanh.nn.63cntt@ntu.edu.vn>
2024-03-25 12:48:35 +0700

    Xử lý sự kiện để chuyển màn hình

--- Cau3_AppGioiThieu/app/src/main/java/ntu/mssv63131268/MainActivity.java
+++ Cau3_AppGioiThieu/app/src/main/java/ntu/mssv63131268/MainActivity.java
@@ -1,6 +1,9 @@
 package ntu.mssv63131268;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +19,16 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        //Chuyển màn hình sang màn hình thông tin
+        Button buttonTT = findViewById(R.id.btnXem);
+        buttonTT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Chuyển màn hình bằng intent
+                Intent intentTT = new Intent(MainActivity.this, ThongTinSV.class);
+                startActivity(intentTT);
+            }
+        });
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
