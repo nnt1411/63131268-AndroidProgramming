ae60a1020c5c8453976b31b4ce4c52816c2f76cb
nnt1411 <thanh.nn.63cntt@ntu.edu.vn>
2024-04-16 07:55:39 +0700

    ViewPager2: Thêm hiệu ứng chuyển đổi trang thu nhỏ bằng PageTransformer

--- ViewPager2RecylerView/app/src/main/java/thanh/edu/viewpager2recylerview/MainActivity.java
+++ ViewPager2RecylerView/app/src/main/java/thanh/edu/viewpager2recylerview/MainActivity.java
@@ -1,6 +1,7 @@
 package thanh.edu.viewpager2recylerview;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -29,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
         viewPager2Land = findViewById(R.id.vp2Land);
         landScapeAdapter = new LandScapeAdapter(this,viewPagerDatas);
         viewPager2Land.setAdapter(landScapeAdapter);
+        //PageTransformer
+        viewPager2Land.setPageTransformer(new ZoomOutPageTransformer());
         viewPager2Land.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
@@ -60,5 +63,38 @@ public class MainActivity extends AppCompatActivity {
         dsDuLieu.add(new LandScape("tom", "MÈO TOM"));
         dsDuLieu.add(new LandScape("jerry", "CHUỘT JERRY"));
         return dsDuLieu;
+    }
+
+    private class ZoomOutPageTransformer implements ViewPager2.PageTransformer {
+        private static final float MIN_SCALE = 0.85f;
+        private static final float MIN_ALPHA = 0.5f;
+
+        @Override
+        public void transformPage(View view, float position) {
+            int pageWidth = view.getWidth();
+            int pageHeight = view.getHeight();
+
+            if (position < -1) {
+                view.setAlpha(0f);
+            } else if (position <= 1) {
+                float scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position));
+                float vertMargin = pageHeight * (1 - scaleFactor) / 2;
+                float horzMargin = pageWidth * (1 - scaleFactor) / 2;
+                if (position < 0) {
+                    view.setTranslationX(horzMargin - vertMargin / 2);
+                } else {
+                    view.setTranslationX(-horzMargin + vertMargin / 2);
+                }
+
+                view.setScaleX(scaleFactor);
+                view.setScaleY(scaleFactor);
+
+                view.setAlpha(MIN_ALPHA +
+                        (scaleFactor - MIN_SCALE) /
+                                (1 - MIN_SCALE) * (1 - MIN_ALPHA));
+            } else {
+                view.setAlpha(0f);
+            }
+        }
     }
 }
