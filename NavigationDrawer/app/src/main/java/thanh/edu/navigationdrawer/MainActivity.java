0a2f47db38e2e1550f4f85c996852dd60c07b1f3
nnt1411 <thanh.nn.63cntt@ntu.edu.vn>
2024-05-08 13:17:23 +0700

    Bài navigation drawer

---
+++ NavigationDrawer/app/src/main/java/thanh/edu/navigationdrawer/MainActivity.java
@@ -0,0 +1,37 @@
+package thanh.edu.navigationdrawer;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+public class MainActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    ImageButton imageButtonDrawer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.main);
+        imageButtonDrawer = findViewById(R.id.imageButtonDrawer);
+        imageButtonDrawer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.open();
+            }
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
