63be8b28a7ec0f991a64e909cd93a89f240fd2f0
nnt1411 <thanh.nn.63cntt@ntu.edu.vn>
2024-05-08 12:31:49 +0700

    Bài tập bottom navigation

---
+++ BottomNavigationView/app/src/main/java/thanh/edu/bottomnavigationview/MainActivity.java
@@ -0,0 +1,40 @@
+package thanh.edu.bottomnavigationview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AlgorithmFragment()).commit();
+    }
+    private final BottomNavigationView.OnNavigationItemSelectedListener navListener = item -> {
+        Fragment selectedFragment = null;
+        int itemId = item.getItemId();
+        if (itemId == R.id.algorithm) {
+            selectedFragment = new AlgorithmFragment();
+        } else if (itemId == R.id.course) {
+            selectedFragment = new CourseFragment();
+        } else if (itemId == R.id.profile) {
+            selectedFragment = new ProfileFragment();
+        }
+        if (selectedFragment != null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+        }
+        return true;
+    };
+}
